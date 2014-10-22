8330e88613c6e020906638da3e9d7a23a57a1dd7
David D. Meyer <meyerdave4@gmail.com>
2014-10-21 22:48:20 -0700

    Add program to determine if two strings are anagrams or not

---
+++ Anagrams.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool checkAnagram(string a, string b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return (a == b); // Doesn't matter if the strings change within this function because we pass by value.
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: ./progam_name string1 string2" << endl;
+        return -1;
+    }
+    string a = argv[1];
+    string b = argv[2];
+    bool is_anagram = checkAnagram(a, b);
+    cout << is_anagram << endl;
+    return 0;
+}
